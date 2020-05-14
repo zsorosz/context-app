fd5919abebc157e8ef1e68b5f2895a24408aae7e
Zsofia <oroszi.zsofi@gmail.com>
2020-05-14 13:25:44 +0200

    Add Form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Form from './Form';
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Form />
+    </>
   );
 }
 
---
+++ src/Form.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles/FormStyles';
+
+
+class Form extends Component {
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <main className={classes.main}>
+                <Paper className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography variant="h5">Sign In</Typography>
+                    <Select value="english">
+                        <MenuItem value="english">English</MenuItem>
+                        <MenuItem value="german">German</MenuItem>
+                        <MenuItem value="hungarian">Hungarian</MenuItem>
+                    </Select>
+                    <form className={classes.form}>
+                        <FormControl margin="normal" required fullWidth>
+                            <InputLabel htmlFor="email">Email</InputLabel>
+                            <Input id="email" name="email" autoFocus></Input>
+                        </FormControl>
+                        <FormControl margin="normal" required fullWidth>
+                            <InputLabel htmlFor="password">Password</InputLabel>
+                            <Input id="password" name="password" autoFocus></Input>
+                        </FormControl>
+                        <FormControlLabel control={<Checkbox color="primary" />} label="Remember Me" />
+                        <Button variant="contained" type="submit" color="primary" fullWidth className={classes.submit}>Sign In</Button>
+                    </form>
+                </Paper>
+            </main>
+        )
+    }
+}
+
+export default withStyles(styles)(Form);
