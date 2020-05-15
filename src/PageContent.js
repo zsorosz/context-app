8b25bbf2053103df7cb141235c7eea28f2b813fd
Zsofia <oroszi.zsofi@gmail.com>
2020-05-15 21:18:09 +0200

    Refactor PageContent with Hooks

--- src/PageContent.js
+++ src/PageContent.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from './contexts/ThemeContext';
 
-export default class PageContent extends Component {
-    static contextType = ThemeContext;
-    render() {
-        const { isDarkMode } = this.context;
-        const styles = {
-            width: "100vw",
-            height: "100vh",
-            backgroundColor: isDarkMode ? "black" : "white"
-        }
-        return (
-            <div style={styles}>{this.props.children}</div>
-        )
+export default function PageContent(props){
+    const { isDarkMode } = useContext(ThemeContext);
+    const styles = {
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: isDarkMode ? "black" : "white"
     }
-};
+    return <div style={styles}>{props.children}</div>
+}
+
+// export default class PageContent extends Component {
+//     static contextType = ThemeContext;
+//     render() {
+//         const { isDarkMode } = this.context;
+//         const styles = {
+//             width: "100vw",
+//             height: "100vh",
+//             backgroundColor: isDarkMode ? "black" : "white"
+//         }
+//         return (
+//             <div style={styles}>{this.props.children}</div>
+//         )
+//     }
+// };
 
